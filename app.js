6666144adb3f5538d932922aef52f9e924d5346e
kkrizek06 <96017493+kkrizek06@users.noreply.github.com>
2022-03-14 20:29:58 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// import data from table.js
+const tableData = data;
+
+// reference HTML table using D3
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // this clears existing table
+    tbody.html("");
+}
+
+//for each loops
+data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+    });
+});
+
+function handleClick() {
+    //grab datetime value form filter. Def variable
+    let date = d3.select("#datetime").property("value")
+    let filteredData = tableData;
+
+    //check dat was entered and filer the data. Create conditional 
+    if (date){
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    //rebuild table using 
+    buildTable(filteredData)
+};
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+buildTable(tableData);
